c2f864ffe7d27df7328e3b6c057cffe97f19f62d
johnlopez0505 <lopezcon1@hotmail.com>
2023-10-23 12:45:50 +0200

    project

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.john.proyecto1"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.john.proyecto1"
--- app/src/main/java/com/john/proyecto1/activitySecund.kt
+++ app/src/main/java/com/john/proyecto1/activitySecund.kt
@@ -3,9 +3,9 @@ package com.john.proyecto1
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class activitySecund : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_secund)
     }
 }
