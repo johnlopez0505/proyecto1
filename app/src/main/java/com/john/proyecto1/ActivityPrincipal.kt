43fa187b25c48c22779b9215a01888569e81ec75
johnLopez0505 <lopezcon1@hotmail.com>
2023-10-24 23:14:00 +0200

    version 0.0.2

--- app/src/main/java/com/john/proyecto1/ActivityPrincipal.kt
+++ app/src/main/java/com/john/proyecto1/ActivityPrincipal.kt
@@ -1,45 +1,73 @@
 package com.john.proyecto1
 
-import android.app.SearchManager
 import android.content.Intent
-import android.content.pm.PackageManager
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.AlarmClock
 import android.widget.ImageButton
+import android.widget.TextView
+import android.widget.Toast
 
 class ActivityPrincipal : AppCompatActivity() {
 
-
-    private lateinit var buttonCall: ImageButton
-    private lateinit var buttonUrl : ImageButton
+    private lateinit var buttonCall  : ImageButton
+    private lateinit var buttonUrl   : ImageButton
+    private lateinit var buttonAlarm : ImageButton
+    private lateinit var buttonEmail : ImageButton
     private lateinit var intent : Intent
     companion object{
-        const val query = "google"
+        const val url = "https://www.google.com/"
     }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_principal)
+
         initEvent()
     }
 
     private fun initEvent() {
         buttonCall = findViewById(R.id.btn_call)
+        buttonUrl = findViewById(R.id.btn_url)
+        buttonAlarm = findViewById(R.id.btn_alarma)
+        buttonEmail = findViewById(R.id.btn_email)
+        val message = "despertar"
+        val hour = 7
+        val minutes = 30
 
-        buttonCall.setOnClickListener { view ->
+        buttonCall.setOnClickListener {
             intent = Intent(this, activitySecund::class.java).apply {
                 putExtra("name", "LLamar a Emergencias")
             }
             startActivity(intent)
         }
-        buttonUrl = findViewById(R.id.btn_url)
-        buttonUrl.setOnClickListener{view ->
-            intent = Intent(Intent.ACTION_WEB_SEARCH).apply {
-                intent.putExtra(SearchManager.QUERY,query)
+
+        buttonUrl.setOnClickListener{
+            intent = Intent(Intent.ACTION_VIEW).apply {
+                data = Uri.parse(url)
             }
-            if(intent.resolveActivity(packageManager) != null){
-                startActivity(intent)
-            }
+            startActivity(intent)
         }
+
+        buttonAlarm.setOnClickListener{
+            intent = Intent(AlarmClock.ACTION_SET_ALARM).apply{
+                putExtra(AlarmClock.EXTRA_MESSAGE,message)
+                putExtra(AlarmClock.EXTRA_HOUR,hour)
+                putExtra(AlarmClock.EXTRA_MINUTES,minutes)
+            }
+            startActivity(intent)
+        }
+
+
+        val subject = "saludo"
+        val content = "Hola clase PMP 23/24 "
+        buttonEmail.setOnClickListener{
+            intent = Intent(Intent.ACTION_SENDTO, Uri.fromParts("mailto","clase2Dam@educand.es",null)).apply {
+                putExtra(Intent.EXTRA_SUBJECT,subject)
+                putExtra(Intent.EXTRA_TEXT,content)
+            }
+            startActivity(intent)
+        }
+
     }
 }
